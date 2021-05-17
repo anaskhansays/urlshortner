f352240e17a981105aef994fecec778972fc5c22
Anas Khan <anas75313@gmail.com>
2021-05-17 19:12:34 +0530

    Update database.sql

--- database.sql
+++ database.sql
@@ -1,4 +1,4 @@
-CREATE TABLE `short_urls` (
+CREATE TABLE `shortened_urls` (
  `id` int(11) NOT NULL AUTO_INCREMENT,
  `long_url` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
  `short_code` varchar(25) COLLATE utf8_unicode_ci NOT NULL,
